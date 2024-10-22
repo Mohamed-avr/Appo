68546825f07ffd50077cff2b25530279e769a17f
Mohamed <mouh4945@gmail.com>
2024-10-22 17:28:15 +0100

    update

--- App.js
+++ App.js
@@ -1,50 +1,109 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-  ActivityIndicator,
-  Alert,
-  TextBase,
-} from "react-native";
-import Greeting from "./components/Greeting";
-import Box from "./components/Box";
+import { View, SafeAreaView, Text, Platform, ScrollView } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
-  const [isActive, setIsActive] = useState(true);
+  // seperated data for signle maping
+  const charmanderData = {
+    name: "Charmander",
+    image: require("./assets/pokemons/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/pokemons/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/pokemons/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pokemons/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
+
+  //  all data in one : for mapping
+  const pokemonData = [
+    {
+      name: "Charmander",
+      image: require("./assets/pokemons/charmander.png"),
+      type: "Fire",
+      hp: 39,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Water", "Rock"],
+    },
+    {
+      name: "Squirtle",
+      image: require("./assets/pokemons/squirtle.png"),
+      type: "Water",
+      hp: 44,
+      moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+      weaknesses: ["Electric", "Grass"],
+    },
+    {
+      name: "Bulbasaur",
+      image: require("./assets/pokemons/bulbasaur.png"),
+      type: "Grass",
+      hp: 45,
+      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    },
+    {
+      name: "Pikachu",
+      image: require("./assets/pokemons/pikachu.png"),
+      type: "Electric",
+      hp: 35,
+      moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+      weaknesses: ["Ground"],
+    },
+  ];
+
   return (
-    <View
+    <SafeAreaView
       style={{
-        padding: 16,
-        backgroundColor: "#111",
+        paddingTop: Platform.OS === "ios" ? 0 : 0,
+        paddingHorizontal: Platform.OS === "android" ? 0 : 0,
+        marginTop: 0,
         flex: 1,
-        borderWidth: 2,
-        marginTop: 40,
-        borderColor: "red",
+        backgroundColor: "#111",
       }}
     >
-      <Box
+      <ScrollView
         style={{
-          backgroundColor: "red",
-          postion: "absolute",
-          top: 150,
-          left: 150,
+          marginTop: 40,
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        BOX 1
-      </Box>
-      <Box style={{ backgroundColor: "#FCC", postion: "absolute", top: -150 }}>
-        BOX 2
-      </Box>
-      <Box style={{ backgroundColor: "#ff0" }}>BOX 1</Box>
-      <Box style={{ backgroundColor: "#F8C" }}>BOX 2</Box>
-      <Box style={{ backgroundColor: "#ee9" }}>BOX 1</Box>
-    </View>
+        {pokemonData.map((pokemon, index) => {
+          return <PokemonCard key={index} {...pokemon} />;
+        })}
+      </ScrollView>
+
+      {/* 
+      sigle data mapping
+      <PokemonCard {...charmanderData} />
+      <PokemonCard {...squirtleData} />
+      <PokemonCard {...bulbasaurData} />
+      <PokemonCard {...pikachuData} /> 
+      
+      */}
+    </SafeAreaView>
   );
 }
-
