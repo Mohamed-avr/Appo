6006c99b193921c86958adaf0d812e9ca6353e0d
Mohamed <mouh4945@gmail.com>
2024-11-26 02:06:24 +0100

    update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   View,
   SafeAreaView,
@@ -7,102 +8,61 @@ import {
   ScrollView,
   FlatList,
   SectionList,
+  StatusBar,
+  TextInput,
 } from "react-native";
 
-import { StatusBar } from "expo-status-bar";
-
-const Data = [
-  {
-    type: "Grass",
-    data: ["Bulbasaur", "Ivysaur", "Venusaur"],
-  },
-  {
-    type: "Fire",
-    data: ["Charmander", "Charmeleon", "Charizard"],
-  },
-  {
-    type: "Water",
-    data: ["Squirtle", "Wartortle", "Blastoise"],
-  },
-  { type: "Electric", data: ["Pikachu", "Raichu"] },
-];
-
 export default function App() {
-  console.log(Data);
+  const [name, setName] = useState("");
   return (
     <SafeAreaView
       style={{
         paddingTop: Platform.OS === "ios" ? 24 : 36,
         paddingHorizontal: Platform.OS === "android" ? 16 : 16,
-        backgroundColor: "#000",
+        backgroundColor: "#fff",
         flex: 1,
       }}
     >
-      <SectionList
-        sections={Data}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => {
-          return (
-            <View
-              style={{
-                backgroundColor: "#fff",
-                padding: 10,
-                width: "100%",
-                borderRadius: 0,
-                marginTop: 10,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 16,
-                  color: "#000",
-                  fontWeight: "medium",
-                }}
-              >
-                {item}
-              </Text>
-            </View>
-          );
-        }}
-        renderSectionHeader={({ section: { type } }) => (
-          <Text
-            style={{
-              fontSize: 20,
-              color: "#fff",
-              fontWeight: "bold",
-              color: "#fff",
-              padding: 10,
-              marginTop: 10,
-            }}
-          >
-            {type}
-          </Text>
-        )}
-        renderSectionFooter={({ section }) => {
-          return (
-            <View
-              style={{
-                backgroundColor: "#000",
-                padding: 10,
-                width: "100%",
-                borderRadius: 0,
-                marginTop: 6,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: "#fff",
-                  fontWeight: "medium",
-                }}
-              >
-                Total: {section.data.length}
-              </Text>
-            </View>
-          );
-        }}
+      <TextInput
+        style={styles.textInput}
+        value={name}
+        onChangeText={setName}
+        placeholder="enter your name"
+        secureTextEntry={true}
+        autoCapitalize="characters"
       />
+
+      <TextInput
+        style={[styles.textInput, styles.textInputMl]}
+        value={name}
+        onChangeText={setName}
+        placeholder="enter your name"
+      />
+
+      <Text style={styles.text}> Your name is: {name}ačić </Text>
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  textInput: {
+    marginTop: 32,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+
+  text: {
+    fontSize: 24,
+    color: "#222",
+    fontWeight: "bold",
+    marginTop: 24,
+  },
+  textInputMl: {
+    // textAlignVertical: "top",
+    minHeight: 100,
+  },
+});
