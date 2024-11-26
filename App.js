4aa72ff29f17748b6d55cd2141df298d49afbd3f
Mohamed <mouh4945@gmail.com>
2024-11-26 02:19:41 +0100

    update

--- App.js
+++ App.js
@@ -10,59 +10,60 @@ import {
   SectionList,
   StatusBar,
   TextInput,
+  Switch,
 } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <SafeAreaView
       style={{
         paddingTop: Platform.OS === "ios" ? 24 : 36,
         paddingHorizontal: Platform.OS === "android" ? 16 : 16,
-        backgroundColor: "#fff",
+        backgroundColor: isDarkMode ? "#222" : "#fff",
         flex: 1,
       }}
     >
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder="enter your name"
-        secureTextEntry={true}
-        autoCapitalize="characters"
+      <Switch
+        value={isDarkMode}
+        onValueChange={(newValue) => setIsDarkMode(newValue)}
       />
-
-      <TextInput
-        style={[styles.textInput, styles.textInputMl]}
-        value={name}
-        onChangeText={setName}
-        placeholder="enter your name"
-      />
-
-      <Text style={styles.text}> Your name is: {name}ačić </Text>
+      <Text
+        style={[
+          styles.SwitchComponentHeading,
+          {
+            color: isDarkMode ? "#fff" : "#222",
+          },
+        ]}
+      >
+        The theme is: {isDarkMode ? "Dark" : "Light"}
+      </Text>
+      <Text
+        style={[
+          styles.DarkModeLabel,
+          {
+            color: isDarkMode ? "#fff" : "#222",
+          },
+        ]}
+      >
+        {isDarkMode ? "Dark Mode" : "Light Mode"}
+      </Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    marginTop: 32,
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 8,
-    borderRadius: 8,
-    marginBottom: 16,
-  },
-
-  text: {
-    fontSize: 24,
+  SwitchComponentHeading: {
+    fontSize: 39,
     color: "#222",
     fontWeight: "bold",
     marginTop: 24,
   },
-  textInputMl: {
-    // textAlignVertical: "top",
-    minHeight: 100,
+
+  DarkModeLabel: {
+    fontSize: 24,
+    color: "#222",
+    marginTop: 24,
   },
 });
