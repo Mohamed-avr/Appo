becc730fb69916766e507d0313a30f838242c213
Mohamed <mouh4945@gmail.com>
2024-12-03 08:08:57 +0100

    update

--- App.js
+++ App.js
@@ -11,45 +11,44 @@ import {
   StatusBar,
   TextInput,
   Switch,
+  Button,
 } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <SafeAreaView
+    <View
       style={{
         paddingTop: Platform.OS === "ios" ? 24 : 36,
         paddingHorizontal: Platform.OS === "android" ? 16 : 16,
-        backgroundColor: isDarkMode ? "#222" : "#fff",
+        backgroundColor: "#f0f0f0",
         flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
       }}
     >
-      <Switch
-        value={isDarkMode}
-        onValueChange={(newValue) => setIsDarkMode(newValue)}
-      />
-      <Text
-        style={[
-          styles.SwitchComponentHeading,
-          {
-            color: isDarkMode ? "#fff" : "#222",
-          },
-        ]}
-      >
-        The theme is: {isDarkMode ? "Dark" : "Light"}
-      </Text>
-      <Text
-        style={[
-          styles.DarkModeLabel,
-          {
-            color: isDarkMode ? "#fff" : "#222",
-          },
-        ]}
-      >
-        {isDarkMode ? "Dark Mode" : "Light Mode"}
-      </Text>
-    </SafeAreaView>
+      <View style={styles.form}>
+        <Text style={styles.label}> Username</Text>
+        <TextInput
+          style={styles.input}
+          value={userName}
+          placeholder="Enter your name"
+          onChangeText={setUserName}
+        />
+
+        <Text style={styles.label}> Passowrd</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          placeholder="Enter your passowrd"
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        />
+
+        <Button style={styles.login} title="Submit" />
+      </View>
+    </View>
   );
 }
 
@@ -61,9 +60,37 @@ const styles = StyleSheet.create({
     marginTop: 24,
   },
 
-  DarkModeLabel: {
-    fontSize: 24,
-    color: "#222",
-    marginTop: 24,
+  form: {
+    padding: 16,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+  },
+  input: {
+    height: 48,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 5,
+  },
+  label: {
+    marginTop: 10,
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  login: {
+    backgroundColor: "#000",
+    color: "white",
+    padding: 10,
+    color: "#f194ff",
   },
 });
